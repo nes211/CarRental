490103b245298a08b45a74f0ce952d60c7547c77
TomTD <nes211nes211nes@gmail.com>
2024-05-21 12:59:30 +0200

    AuthController Test update 85%

--- src/test/java/pl/tdelektro/CarRental/Auth/AuthControllerTest.java
+++ src/test/java/pl/tdelektro/CarRental/Auth/AuthControllerTest.java
@@ -2,6 +2,8 @@ package pl.tdelektro.CarRental.Auth;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import jakarta.transaction.Transactional;
 import org.junit.After;
 import org.junit.Before;
@@ -12,7 +14,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.annotation.Order;
 
 import static org.hamcrest.Matchers.notNullValue;
-
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
@@ -21,13 +26,11 @@ public class AuthControllerTest {
 
     private static final String userName = "testuser@test.test";
 
-
     @Before
     public void warmUp() {
 
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 8080;
-
 
         String newUserString = """
                 {
@@ -72,6 +75,7 @@ public class AuthControllerTest {
                 "password":"test"
                 }
                 """;
+
         RestAssured
                 .given()
                 .contentType(ContentType.JSON)
@@ -99,19 +103,46 @@ public class AuthControllerTest {
                 }
                 """;
 
-        RestAssured.given().log().all()
+        Response response = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
                 .body(userString)
                 .when()
                 .post("/auth/authenticate")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .extract().response();
 
-//        assertThat(token, notNullValue());
+        JsonPath jsonPath = response.jsonPath();
+        String token = jsonPath.getString("token");
+        assertNotNull(token);
+        assertFalse(token.isEmpty());
     }
 
     @Test
     @Order(3)
+    public void authenticateFailedTest() {
+        String userString = """
+                {
+                "emailAddress":"testuser2@test.test",
+                "password" : "test"
+                }
+                """;
+
+        Response response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(userString)
+                .when()
+                .post("/auth/authenticate")
+                .then()
+                .statusCode(401)
+                .extract().response();
+
+        assertFalse(response.getStatusCode()==200);
+    }
+
+    @Test
+    @Order(4)
     public void deleteCustomer() {
 
         String path = "/auth/deleteUser/" + userName;
